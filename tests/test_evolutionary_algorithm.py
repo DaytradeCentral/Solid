98552ba93296cf66ab54a99de769dcf5a4200a70
James Powell <james@dontusethiscode.com>
2017-06-25 22:05:46 -0400

    EvolutionaryAlgorithm: improve test

--- tests/test_evolutionary_algorithm.py
+++ tests/test_evolutionary_algorithm.py
@@ -1,33 +1,39 @@
-from random import choice, randint, random
+from random import choice, randrange, random
 from string import ascii_lowercase
 from Solid.EvolutionaryAlgorithm import EvolutionaryAlgorithm
 from unittest import TestCase, main
 
 class Algorithm(EvolutionaryAlgorithm):
     """
-    Tries to get a randomly-generated string to match string "clout"
+    Tries to get a randomly-generated string to match some word
     """
+    def __init__(self, word, *args, **kwargs):
+        self.word = word
+        super(Algorithm, self).__init__(*args, **kwargs)
+
     def _initial_population(self):
-        return list(''.join([choice(ascii_lowercase) for _ in range(5)]) for _ in range(50))
+        return [''.join(choice(ascii_lowercase) for _ in self.word)
+                for _ in range(50)]
 
     def _fitness(self, member):
-        return float(sum(member[i] == "clout"[i] for i in range(5)))
+        return sum(x == y for x, y in zip(self.word, member))
 
     def _crossover(self, parent1, parent2):
-        partition = randint(0, len(self.population[0]) - 1)
-        return parent1[0:partition] + parent2[partition:]
+        index = randrange(len(self.word))
+        return parent1[:index] + parent2[index:]
 
     def _mutate(self, member):
         if self.mutation_rate >= random():
-            member = list(member)
-            member[randint(0,4)] = choice(ascii_lowercase)
-            member = ''.join(member)
+            index = randrange(len(self.word))
+            return member[:index] + choice(ascii_lowercase) + member[index+1:]
         return member
 
 class TestEvolutionaryAlgorithm(TestCase):
-    def test_algorithm(_):
-        algorithm = Algorithm(.5, .7, 500, max_fitness=None)
+    def test_algorithm(self):
+        word = 'clout'
+        algorithm = Algorithm(word, .5, .7, 500, max_fitness=None)
         algorithm.run()
+        self.assertEqual(algorithm.best_member, word)
 
 if __name__ == '__main__':
     from logging import basicConfig, INFO
